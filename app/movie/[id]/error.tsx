fad2e25d6b072d7228fe7ead5b88339026e4dc73
WintBree <u.duulatov@gmail.com>
2025-06-28 10:09:32 +0600

    error tsx for movie page

---
+++ app/movie/[id]/error.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Button } from "@/src/shared/ui/button";
+import { BackButton } from "@/src/shared/ui/sub/BackButton";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error("App error:", error);
+  }, [error]);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[60vh] text-center px-4">
+      <h2 className="text-2xl font-semibold text-destructive mb-4">
+        No movie found
+      </h2>
+      <p className="text-gray-700 mb-6">
+        We’re sorry! Seems like the URL is incorrect.
+      </p>
+      <div className="flex flex-col sm:flex-row items-center gap-4">
+        <BackButton />
+
+        <Button onClick={() => reset()}>Try again</Button>
+      </div>
+    </div>
+  );
+}
--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,5 +1,22 @@
-import MoviePage from "@/src/pages/MoviePage";
+import { fetchMovieById } from "@/src/entities/movie/api/fetchMovieById";
+import { fetchMovieCast } from "@/src/entities/movie/api/fetchMovieCast";
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <MoviePage params={params} />;
+import { MovieDetails } from "@/src/widgets/movie-details/ui/MovieDetails";
+
+import { BackButton } from "@/src/shared/ui/sub/BackButton";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const movieId = params.id;
+
+  const [movie, cast] = await Promise.all([
+    fetchMovieById(movieId),
+    fetchMovieCast(movieId),
+  ]);
+
+  return (
+    <main className="max-w-5xl mx-auto p-4 space-y-4">
+      <BackButton />
+      <MovieDetails movie={movie} cast={cast} />
+    </main>
+  );
 }
