7d41731f3330b401d847f6638aee1c42b31db147
WintBree <u.duulatov@gmail.com>
2025-06-28 11:33:23 +0600

    nextConfig path configured

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     domains: ["image.tmdb.org"],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
   },
 };
 
